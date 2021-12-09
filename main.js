f396b0ed779e6be8f7181f21eb41817b490e86b0
Owen Manmano <Bigowen27@gmail.com>
2021-12-09 15:12:30 -0700

    Thursday-Lab-BossFight

---
+++ main.js
@@ -0,0 +1,20 @@
+let attacks = {
+  
+}
+
+
+
+
+
+function buttonPunch() {
+console.log('message')
+}
+
+function buttonKick() {
+  console.log('message')
+}
+
+function buttonPunch() {
+  console.log ('message')
+}
+
