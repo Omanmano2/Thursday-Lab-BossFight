d6d0eb651be79f1f14ebdb12f0a06c06e52ac0bd
Owen Manmano <Bigowen27@gmail.com>
2021-12-09 22:43:16 -0700

    Thursday-Lab-Bossfight

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-let attacks = {
-  
+const hero = {
+  health: 100
 }
 
+const boss = {
+  health: 100
+}
 
+let attacks = {
+  punch: {
+    damage: 2,
+  },
+
+  kick: {
+    damage: 5,
+  },
+
+  weapon: {
+  damage: 10,
+  }
+}
 
 
 
 function buttonPunch() {
-console.log('message')
+const punchDamage = attacks.punch.damage
+boss.health -= punchDamage
+  console.log(boss.health)
+  document.getElementById('bossProgress').style = `width: ${boss.health}%`
 }
+
 
 function buttonKick() {
-  console.log('message')
+const kickDamage = attacks.kick.damage
+boss.health -= kickDamage
+  console.log(boss.health)
+  document.getElementById('bossProgress').style = `width: ${boss.health}%`
 }
 
-function buttonPunch() {
-  console.log ('message')
+function buttonWeapon() {
+  const weaponDamage = attacks.weapon.damage
+  boss.health -= weaponDamage
+  console.log(boss.health)
+  document.getElementById('bossProgress').style = `width: ${boss.health}%`
 }
 
+function drawMain(){
+  template = ''
+  // for(const key in )
+
+}
